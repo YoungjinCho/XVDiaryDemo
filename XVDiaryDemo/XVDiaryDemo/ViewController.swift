8ad34666e8b53b490e71bef4da262535c64588d1
xeve <xeve.cyj@gmail.com>
2018-08-27 01:21:39 +0900

    Init Project

---
+++ XVDiaryDemo/XVDiaryDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XVDiaryDemo
+//
+//  Created by Cho Youngjin on 27/08/2018.
+//  Copyright © 2018 xeve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
